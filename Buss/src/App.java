fd2cc835a31d09b4c835d12f807af9169788f9e6
Korven13 <hedav05@gmail.com>
2023-03-20 13:24:46 +0100

    1

---
+++ Buss/src/App.java
@@ -0,0 +1,13 @@
+public class App {
+
+    static void sittplatser() {
+        for (int i=1; i<21; i++) {
+            if ()
+        }
+    }
+
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
