8bdba4d4b89282240602698dac9be4eac34eb8bc
Korven13 <hedav05@gmail.com>
2023-03-23 16:19:35 +0100

    Update App.java

--- Buss/src/App.java
+++ Buss/src/App.java
@@ -1,13 +1,34 @@
 public class App {
 
+    static int[][] plats ={
+        {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
+    }
+    ;
     static void sittplatser() {
-        for (int i=1; i<21; i++) {
-            if ()
+        for (int i=0; i<21; i++) {
+            if (plats[1][i] == 1) {
+                System.out.print("b");
+            }
+            else {
+                System.out.print(plats[0][i]);
+            }
+            if (plats[0][i]%4==0 && plats[0][i]!=20) {
+                System.out.println();
+            }
+            else if (plats[0][i]%4==2 && plats[0][i]!=18) {
+                System.out.print("   ");
+            }
+            else {
+                System.out.print("  ");
+            }
         }
     }
 
 
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.print("\033[H\033[2J");  
+        sittplatser();
     }
 }
