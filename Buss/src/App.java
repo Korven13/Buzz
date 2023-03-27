f9db27af55eef0ebab282e22cf692d75c9a21bcd
Korven13 <hedav05@gmail.com>
2023-03-27 13:26:11 +0200

    2

--- Buss/src/App.java
+++ Buss/src/App.java
@@ -1,11 +1,12 @@
 public class App {
-
+//2D fält för nummret på sittplatserna och tomma platser för bokning och personummer.
     static int[][] plats ={
         {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
         {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-    }
-    ;
+    };
+    
+//Algoritm för att printa ut sittplatserna visuellt.
     static void sittplatser() {
         for (int i=0; i<21; i++) {
             if (plats[1][i] == 1) {
@@ -18,17 +19,37 @@ public class App {
                 System.out.println();
             }
             else if (plats[0][i]%4==2 && plats[0][i]!=18) {
-                System.out.print("   ");
+                if (plats[0][i]+1<10) {
+                    System.out.print("    ");
+                }
+                else {
+                    System.out.print("    ");
+                }
             }
             else {
-                System.out.print("  ");
+                if (plats[0][i]+1<11) {
+                    System.out.print("  ");
+                    if (plats[0][i]+1<9) {
+                        System.out.print(" ");
+                        
+                    }
+                }
+                else {
+                    System.out.print(" ");
+                }
+
             }
         }
     }
-
+//Välj sittplats.
+    static void val() {
+        System.out.println("\r\n" + "Välj en ledig plats");
+    }
 
     public static void main(String[] args) throws Exception {
         System.out.print("\033[H\033[2J");  
         sittplatser();
+        val();
+
     }
 }
